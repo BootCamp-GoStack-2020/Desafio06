b47a80421f235109230bab3fc625026cead5d7e2
viannaandreBR <vianna_andre@hotmail.com@users.noreply.github.com>
2020-08-30 18:48:05 -0300

    DB Read & Total of Income / OutCome

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,18 +1,41 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import { getCustomRepository } from 'typeorm'
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
+// -------------- Método Get -----------------------------------------------------------------
+
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({transactions, balance});
 });
 
+
+// -------------- Método Post -----------------------------------------------------------------
+
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+ 
+  const { title, value, type, category } = request.body;
+  
+  const createTransaction = new CreateTransactionService;
+
+  const transaction = await createTransaction.execute({
+    title, value, type, category 
+  });
+
+  return response.json(transaction)
+
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,11 +1,54 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
 import Transaction from '../models/Transaction';
 
+import Category from '../models/Category';
+
+
+interface  Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ 
+    title, 
+    value, 
+    type, 
+    category
+  }: Request ): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const categoryRepository = getRepository(Category);
+    
+
+    // Verificar se a categoria já existe
+
+
+    // Existe ? Busca no Banco de Dados e usar o id que foi retornado
+
+    // !Existe ? Cria Ela
+
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+    })
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
+
+
 }
 
 export default CreateTransactionService;
